91543726b44deed5c7da0c43f6212eb9c0324798
Solene Rapenne <solene@perso.pw>
2022-12-02 20:45:35 +0100

    add sample issues

---
+++ issues/001.nix
@@ -0,0 +1,26 @@
+let
+  issue = {
+    number = "001";
+
+    links = [
+      ''hello, this is a link to <a href="hello">here</a>''
+    ];
+
+    tips = ''
+
+    '';
+
+    news = [
+      "Wow, such a thing!"
+    ];
+
+    artwork = {
+      caption = "";
+      url = "";
+    };
+
+    events = [
+    ];
+  };
+in
+  issue
---
+++ issues/002.nix
@@ -0,0 +1,24 @@
+let
+  issue = {
+    number = "002";
+
+    links = [
+      ''hello, this is a link to <a href="hello">here</a>''
+    ];
+
+    news = [
+      "Wow, such a thing!"
+    ];
+
+    tips = "";
+
+    artwork = {
+      caption = "";
+      url = "";
+    };
+
+    events = [
+    ];
+  };
+in
+  issue
