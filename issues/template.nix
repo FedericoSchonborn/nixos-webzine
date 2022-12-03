be84f5eadf0071f4e619d0f265e0d9b744dda308
Solene Rapenne <solene@perso.pw>
2022-12-03 19:33:53 +0100

    add an issue template

---
+++ issues/template.nix
@@ -0,0 +1,37 @@
+let
+  issue = {
+    number = "001";
+    date = "2022-12-03";
+
+    news = [
+    ];
+
+    # need some HTML to wrap the content
+    tips = ''
+    '';
+
+    artwork = {
+      caption = "";
+      url = "static/artwork-001.png";
+      author = "";
+    };
+
+    # { text = ""; url = ""; }
+    links = [
+    ];
+
+    # { text = ""; url = ""; }
+    events = [
+    ];
+
+    # need some HTML to wrap the content
+    editors = ''
+    '';
+
+    # list of people who contributed to the issue
+    authors = [
+      "Solène Rapenne"
+    ];
+  };
+in
+  issue
